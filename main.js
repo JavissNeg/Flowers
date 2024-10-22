e62be06978cfd56f68e939fdafab4d577c5b04fa
Javiss Negrete <negretbtino@hotmail.com>
2024-10-21 20:57:16 -0600

    Fix error

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+let isLoaded = false;
+
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('./index.html')
-        .then(response => response.text())
-        .then(data => {
-            document.open();
-            document.write(data);
-            document.close();
-        })
-        .catch(error => console.error('Error loading the HTML file:', error));
+    if (!isLoaded) {
+        fetch('./index.html')
+            .then(response => response.text())
+            .then(data => {
+                document.open();
+                document.write(data);
+                document.close();
+                isLoaded = true;
+            })
+            .catch(error => console.error('Error loading the HTML file:', error));
+    }
 });
