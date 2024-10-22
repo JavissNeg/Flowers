68f80af440a2f2a729ad3f11cb39f71ba7aefa00
Javiss Negrete <negretbtino@hotmail.com>
2024-10-21 20:55:25 -0600

    Fix error

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('index.html')
+        .then(response => response.text())
+        .then(data => {
+            document.open();
+            document.write(data);
+            document.close();
+        })
+        .catch(error => console.error('Error loading the HTML file:', error));
+});
